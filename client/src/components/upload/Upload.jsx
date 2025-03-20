ffe4eb64208b818a247a46db6ed7a42db5c2f15f
Piers Deshmukh <piersdeshmukh@gmail.com>
2025-03-20 12:28:19 +0000

    Update Upload.jsx

--- client/src/components/upload/Upload.jsx
+++ client/src/components/upload/Upload.jsx
@@ -7,7 +7,7 @@ const publicKey = import.meta.env.VITE_IMAGE_KIT_PUBLIC_KEY;
 
 const authenticator =  async () => {
     try {
-        const response = await fetch('http://localhost:3000/auth');
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/auth`);
 
         if (!response.ok) {
             const errorText = await response.text();
