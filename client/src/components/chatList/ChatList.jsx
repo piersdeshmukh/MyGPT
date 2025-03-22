c3ac0c7be9800bab57f3d66f68b24130e6b84cdc
Piers Deshmukh <piersdeshmukh@gmail.com>
2025-03-22 22:05:58 +0000

    Update ChatList.jsx
    explicitly added url

--- client/src/components/chatList/ChatList.jsx
+++ client/src/components/chatList/ChatList.jsx
@@ -6,7 +6,7 @@ const ChatList = () => {
   const { isPending, error, data } = useQuery({
     queryKey: ["userChats"],
     queryFn: () =>
-      fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
+      fetch(`https://piersgpt-server.netlify.app/api/userchats`, {
         credentials: "include",
       }).then((res) => res.json()),
   });
